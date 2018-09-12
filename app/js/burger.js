565ab683a235069a80e95c06c372d37689098362
dankjjk <dankjjk@gmail.com>
2018-09-12 23:15:55 +0900

    Finish

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,12 +1,8 @@
-(function() {
-	var body = document.body;
-	var burgerMenu = document.getElementsByClassName('burger__icon')[0];
-	var burgerContain = document.getElementsByClassName('burger')[0];
-	var burgerNav = document.getElementsByClassName('nav')[0];
-
-  burgerMenu.addEventListener('click', function toggleClasses() {
-    [body, burgerContain, burgerNav].forEach(function (el) {
-      el.classList.toggle('open');
-    });
-  }, false);
-})();
+$('.burger__icon').on('click', function(e) {
+	e.preventDefault;
+	$(this).toggleClass('burger__icon-active');
+	$('.header__logo').toggleClass('header__logo-active');
+	$('.header__block').toggleClass('header__block-active');
+	$('.nav').toggleClass('nav__active');
+	$('.burger').toggleClass('burger__active');
+})
